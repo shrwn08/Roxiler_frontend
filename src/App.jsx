660efc8219e5081b54bf4e3e6df84bce83969a5e
Shrawan <shrwnkumar08@gmail.com>
2025-02-07 01:53:20 +0530

    error fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import "./App.css";
-import Home from "./components/home";
+import Home from "./components/Home";
 
 function App() {
   return (
     <div className="h-full w-full bg-[#E8F9FF] flex justify-center items-center">
       <Home />
-      
-
     </div>
   );
 }
